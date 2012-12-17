ff45c5626cc211a8a35081366f012a2e0c52aa99
Jamie LaMarche <jamielynn5530@gmail.com>
2012-12-17 16:21:01 -0600

    Database Layer
    Fixing tabs and spacing.

--- src/com/modnaut/common/utilities/DatabaseMethods.java
+++ src/com/modnaut/common/utilities/DatabaseMethods.java
@@ -21,200 +21,202 @@ import com.modnaut.common.properties.sqlmetadata.StatementType;
 
 public class DatabaseMethods {
     
-	private static final String SP = "SP";
-	private static final String CALL = "CALL ";
+    private static final String SP = "SP";
+    private static final String CALL = "CALL ";
 	
-	public static ArrayList<String[]> getJustData(String queryName, String queryFile) {
+    public static ArrayList<String[]> getJustData(String queryName, String queryFile) {
 
-	    Connection con = null;
-	    Statement st = null;
-	    ResultSet rs = null;
+	Connection con = null;
+	Statement st = null;
+	ResultSet rs = null;
 
-	    ArrayList<String[]> data = new ArrayList<String[]>();
+	ArrayList<String[]> data = new ArrayList<String[]>();
 	    
-	    try {
+	try {
 	    
+	    con = JdbcConnection.getConnection();
+		
+	    //use statement type object when using static sql with no parameters
+	    st = con.createStatement();
+
+	    Query q = SqlQueries.getQuery(queryName, queryFile);
+
+	    StatementType statement = q.getStatement();
+	    String statementString = statement.getValue();
+		
+	    //check to see if statement is a stored procedure.  This requires additional handling.
+	    if (q.getType().equals(SP))
+		statementString = CALL + statementString;
+
+	    rs = st.executeQuery(statementString);
+
+	    while (rs.next()) {
+    
+		//metadata output of ResultSetObject, allows for retrieving information about the statement dynamically 
+		//without having to know exactly what the statement is.  Puts results into a string array and then a final array to be returned.
+		ResultSetMetaData rsmd = rs.getMetaData();
+		String[] t = new String[rsmd.getColumnCount()];
+		for (int i = 0; t.length > i; i++)
+		    t[i] = rs.getString(i + 1);
+		    data.add(t);
+	    }
+	 } catch (Exception ex) {
+	     ex.printStackTrace();
+	 } finally {
+	
+	     try {
+		 
+		 if (rs != null) {
+		     rs.close();
+		 }
+		
+		 if (st != null) {
+		     st.close();
+		 }
+		
+		 if (con != null) {
+		     con.close();
+		 }
+			
+	     } catch (Exception ex) {
+		    ex.printStackTrace();
+	     }
+	}
+
+	return data;
+    }
+	
+
+    public static ArrayList<String[]> getJustData(String queryName, HashMap<String, String> parms, Connection con, String queryFile) {
+	    
+	//use a prepared statement for sql queries and sps that have input and output parameters.
+	PreparedStatement st = null;
+	ResultSet rs = null;
+
+	ArrayList<String[]> data = new ArrayList<String[]>();
+	    
+	try {
+	
+	    if (con == null)
 		con = JdbcConnection.getConnection();
 		
-		//use statement type object when using static sql with no parameters
-		st = con.createStatement();
-
-		Query q = SqlQueries.getQuery(queryName, queryFile);
-
-		StatementType statement = q.getStatement();
-		String statementString = statement.getValue();
-		
-		//check to see if statement is a stored procedure.  This requires additional handling.
-		if (q.getType().equals(SP))
-		    statementString = CALL + statementString;
-
-		rs = st.executeQuery(statementString);
-
-		while (rs.next()) {
-		    
-		    //metadata output of ResultSetObject, allows for retrieving information about the statement dynamically 
-		    //without having to know exactly what the statement is.  Puts results into a string array and then a final array to be returned.
-		    ResultSetMetaData rsmd = rs.getMetaData();
-		    String[] t = new String[rsmd.getColumnCount()];
-		    for (int i = 0; t.length > i; i++)
-			t[i] = rs.getString(i + 1);
-
-		    data.add(t);
-		}
-		    
-	    } catch (Exception ex) {
-		ex.printStackTrace();
-	    } finally {
-		try {
-		    
-		    if (rs != null) {
-			rs.close();
-		    }
-		
-		    if (st != null) {
-			st.close();
-		    }
-		
-		    if (con != null) {
-			con.close();
-		    }
+	    Query q = SqlQueries.getQuery(queryName, queryFile);
+	    StatementType statement = q.getStatement();
+	    String statementString = statement.getValue();
 			
-		} catch (Exception ex) {
-		    ex.printStackTrace();
+	    if (q.getType().equals(SP))
+		statementString = CALL + statementString;
+
+	    st = con.prepareStatement(statementString);
+	
+	    //grab parameters from sqlmetadata file.  If parameters exist in passed in hashmap, values of the hashmap are used.  
+	    //If they do not exist, the value set the sqlmetadata file will be used.
+	    Parameters parameters = q.getParameters();
+	    List parameterList = parameters.getParameter();
+	    for (int i = 0; parameterList.size() > i; i++)
+	    {
+		Parameter parameter = (Parameter) parameterList.get(i);
+		if (parms.containsKey(parameter.getName())) {
+		    st.setString(parameter.getId().intValue(), parms.get(parameter.getName()));
+		}
+		else {
+		    st.setString(parameter.getId().intValue(), StringUtils.trimToEmpty(parameter.getValue()));
 		}
 	    }
-
-	    return data;
-	}
 	
-	public static ArrayList<String[]> getJustData(String queryName, HashMap<String, String> parms, Connection con, String queryFile) {
-	    
-	    //use a prepared statement for sql queries and sps that have input and output parameters.
-	    PreparedStatement st = null;
-	    ResultSet rs = null;
-
-	    ArrayList<String[]> data = new ArrayList<String[]>();
-	    
-	    try {
-		
-		if (con == null)
-		    con = JdbcConnection.getConnection();
-		
-		Query q = SqlQueries.getQuery(queryName, queryFile);
-		StatementType statement = q.getStatement();
-		String statementString = statement.getValue();
-			
-		if (q.getType().equals(SP))
-			statementString = CALL + statementString;
-
-		st = con.prepareStatement(statementString);
-	
-		//grab parameters from sqlmetadata file.  If parameters exist in passed in hashmap, values of the hashmap are used.  
-		//If they do not exist, the value set the sqlmetadata file will be used.
-		Parameters parameters = q.getParameters();
-		List parameterList = parameters.getParameter();
-		for (int i = 0; parameterList.size() > i; i++)
-		{
-		    Parameter parameter = (Parameter) parameterList.get(i);
-		    if (parms.containsKey(parameter.getName())) {
-			st.setString(parameter.getId().intValue(), parms.get(parameter.getName()));
-		    }
-		    else {
-			st.setString(parameter.getId().intValue(), StringUtils.trimToEmpty(parameter.getValue()));
-		    }
-		}
-	
-		rs = st.executeQuery();
+	    rs = st.executeQuery();
 		    	
-		while (rs.next()) {
+	    while (rs.next()) {
 		    
-		    //metadata output of ResultSetObject, allows for retrieving information about the statement dynamically 
-		    //without having to know exactly what the statement is.  Puts results into a string array and then a final array to be returned.
-		    ResultSetMetaData rsmd = rs.getMetaData();
-		    String[] t = new String[rsmd.getColumnCount()];
-		    for (int i = 0; t.length > i; i++)
-			t[i] = rs.getString(i + 1);
-
+		//metadata output of ResultSetObject, allows for retrieving information about the statement dynamically 
+		//without having to know exactly what the statement is.  Puts results into a string array and then a final array to be returned.
+		ResultSetMetaData rsmd = rs.getMetaData();
+		String[] t = new String[rsmd.getColumnCount()];
+		for (int i = 0; t.length > i; i++)
+		    t[i] = rs.getString(i + 1);
 		    data.add(t);
-		}
-
-	    } catch (Exception ex) {
-		ex.printStackTrace();
-	    } finally {
-		try {
-		    if (st != null) {
-			st.close();
-		    }
-		
-		    if (con != null) {
-			con.close();
-		    }
-			
-		} catch (Exception ex) {
-		    ex.printStackTrace();
-		}
 	    }
-		
-	    return data;
-	}
-	
-	public static int updateData(String queryName, HashMap<String, String> parms, Connection con, String queryFile) {
-	   
-	    //use a prepared statement for sql queries and sps that have input and output parameters.
-	    PreparedStatement st = null;
-	    int row_count = 0;
 	    
+	} catch (Exception ex) {
+	    ex.printStackTrace();
+	} finally {
 	    try {
-		
-		if (con == null)
-		    con = JdbcConnection.getConnection();
-		
-		Query q = SqlQueries.getQuery(queryName, queryFile);
-		StatementType statement = q.getStatement();
-		String statementString = statement.getValue();
-			
-		if (q.getType().equals(SP))
-			statementString = CALL + statementString;
-
-		st = con.prepareStatement(statementString);
-	
-		//grab parameters from sqlmetadata file.  If parameters exist in passed in hashmap, values of the hashmap are used.  
-		//If they do not exist, the value set the sqlmetadata file will be used.
-		Parameters parameters = q.getParameters();
-		List parameterList = parameters.getParameter();
-		for (int i = 0; parameterList.size() > i; i++)
-		{
-		    Parameter parameter = (Parameter) parameterList.get(i);
-		    if (parms.containsKey(parameter.getName())) {
-			st.setString(parameter.getId().intValue(), parms.get(parameter.getName()));
-		    }
-		    else {
-			st.setString(parameter.getId().intValue(), StringUtils.trimToEmpty(parameter.getValue()));
-		    }
+		if (st != null) {
+		    st.close();
 		}
-
-		//executeUpdate() automatically returns row count affected.  If none were affected, zero is returned.
-		row_count = st.executeUpdate();
-
+		
+		if (con != null) {
+		    con.close();
+		}
+			
 	    } catch (Exception ex) {
 		ex.printStackTrace();
-	    } finally {
-		try {
-		    if (st != null) {
-			st.close();
-		    }
+	    }
+	}
 		
-		    if (con != null) {
-			con.close();
-		    }
+	return data;
+    }
+	
+	
+    public static int updateData(String queryName, HashMap<String, String> parms, Connection con, String queryFile) {
+	   
+	//use a prepared statement for sql queries and sps that have input and output parameters.
+	PreparedStatement st = null;
+	int row_count = 0;
+	    
+	try {
+		
+	    if (con == null)
+		con = JdbcConnection.getConnection();
+		
+	    Query q = SqlQueries.getQuery(queryName, queryFile);
+	    StatementType statement = q.getStatement();
+	    String statementString = statement.getValue();
 			
-		} catch (Exception ex) {
-		    ex.printStackTrace();
+	    if (q.getType().equals(SP))
+		statementString = CALL + statementString;
+
+	    st = con.prepareStatement(statementString);
+	
+	    //grab parameters from sqlmetadata file.  If parameters exist in passed in hashmap, values of the hashmap are used.  
+	    //If they do not exist, the value set the sqlmetadata file will be used.
+	    Parameters parameters = q.getParameters();
+	    List parameterList = parameters.getParameter();
+	    for (int i = 0; parameterList.size() > i; i++)
+	    {
+		Parameter parameter = (Parameter) parameterList.get(i);
+		if (parms.containsKey(parameter.getName())) {
+		    st.setString(parameter.getId().intValue(), parms.get(parameter.getName()));
+		}
+		else {
+		    st.setString(parameter.getId().intValue(), StringUtils.trimToEmpty(parameter.getValue()));
 		}
 	    }
- 
-	    return row_count;
+
+	    //executeUpdate() automatically returns row count affected.  If none were affected, zero is returned.
+	    row_count = st.executeUpdate();
+
+	} catch (Exception ex) {
+	    ex.printStackTrace();
+	} finally {
+
+	    try {
+		if (st != null) {
+		    st.close();
+		}
+		
+		if (con != null) {
+		    con.close();
+		}
+			
+	    } catch (Exception ex) {
+		
+		ex.printStackTrace();
+	    }
 	}
+     
+	return row_count;
+    }
 	
 	public static int insertDataReturnId(String queryName, HashMap<String, String> parms, Connection con, String queryFile) {
 	    
