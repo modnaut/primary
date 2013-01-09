907b5c13a089f27a15bbd5a5c30192bc2e411f73
Ben Dalgaard <bendalgaard@gmail.com>
2013-01-09 06:53:21 -0600

    Database Layer
    Initial Logback commit.

--- src/com/modnaut/apps/helloworld/HelloWorldChangeCtrl.java
+++ src/com/modnaut/apps/helloworld/HelloWorldChangeCtrl.java
@@ -6,6 +6,12 @@ import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+
 import com.modnaut.common.framework.FrameworkCtrl;
 import com.modnaut.common.interfaces.ICommonConstants;
 import com.modnaut.common.utilities.DatabaseMethods;
@@ -13,6 +19,8 @@ import com.modnaut.common.utilities.DatabaseMethods;
 public class HelloWorldChangeCtrl extends FrameworkCtrl
 {
 	private static final String XML_FILE = "HelloWorld.xml";
+	private static final Logger logger = LoggerFactory.getLogger("com.modnaut.apps.helloworld.HelloWorldChangeCtrl");
+
 	private static final String GET_ALL_USERS_ALPHABETICALLY = "GET_ALL_USERS_ALPHABETICALLY";
 
 	public HelloWorldChangeCtrl(HttpServletRequest request, HttpServletResponse response)
@@ -25,6 +33,13 @@ public class HelloWorldChangeCtrl extends FrameworkCtrl
 	{
 		try
 		{
+			logger.debug("Hello world.");
+
+			// In order to utilize a different configuration than the default you must place the web/WEB-INF/lib/resource file in the build/classes folder (which is not versioned)
+			// This is not very elegant, but it works for now.
+			LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+			StatusPrinter.print(lc);
+
 			String userList = ICommonConstants.NONE;
 
 			// NOTE: need to run the SP located in the new "primary/sql" folder in MySql before testing
@@ -39,10 +54,10 @@ public class HelloWorldChangeCtrl extends FrameworkCtrl
 					userList += row[0] + ICommonConstants.COMMA;
 					for (int j = 0; row.length > j; j++)
 					{
-						writer.println(row[j]);
+						logger.info(row[j]);
 					}
 				}
-				writer.println(userList);
+				logger.info(userList);
 			}
 
 			marshall(viewMetaData);
@@ -50,6 +65,7 @@ public class HelloWorldChangeCtrl extends FrameworkCtrl
 		}
 		catch (Exception e)
 		{
+			logger.error("This a Logback ERROR meesage.  There was an error in default action of this class.");
 			e.printStackTrace();
 		}
 	}
