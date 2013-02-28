cf8fe6e707408838d9459caa14c6986fdcf0e1d8
Ben Dalgaard <bendalgaard@gmail.com>
2013-02-27 20:47:18 -0600

    Authentication Layer
    Fix SQL Job that will run every hour and delete all sessions older than
    24 hours.

--- sql/SessionCleanerEvent.sql
+++ sql/SessionCleanerEvent.sql
@@ -1,19 +1,22 @@
 
+/**
+
+Job that deletes Sessions that are more than 24 hours old.
+
+SELECT * FROM Common.`session`;
+SHOW Processlist;
+SET GLOBAL event_scheduler = ON;
+SHOW EVENTS;
+
+*/
+
 USE Session;
-
---SHOW Processlist;
-
---SET GLOBAL event_scheduler = ON;
-
---SHOW EVENTS;
 
 DELIMITER $$
 CREATE EVENT IF NOT EXISTS `SessionCleanerEvent`
 	ON SCHEDULE
       EVERY 1 HOUR
-		--STARTS '2013-02-26 03:00:00'
 	DO BEGIN
-		-- Deletes Sessions that are more than 24 hours old.
 		DELETE FROM Session.sessionobject WHERE dateEntered < DATE_SUB(NOW(), INTERVAL 24 HOUR);
-	END */$$
+	END $$
 DELIMITER ;
